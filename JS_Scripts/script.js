4c5e4193159a7f63c62ce3a3b4e1f9fd7b44ae17
10GGGGGGGGGG <47125167+10GGGGGGGGGG@users.noreply.github.com>
2021-01-16 20:03:59 +0100

    publish web

---
+++ JS_Scripts/script.js
@@ -0,0 +1,8 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openSidebar() {
+  document.getElementsByClassName("sidebar")[0].style.left = "0%";
+}
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeSidebar() {
+  document.getElementsByClassName("sidebar")[0].style.left = "-15%";
+}
