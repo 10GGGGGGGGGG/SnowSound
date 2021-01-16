8e670a743e26265de2d76e0d5ab3fd7bc60a6792
10GGGGGGGGGG <47125167+10GGGGGGGGGG@users.noreply.github.com>
2021-01-16 21:31:40 +0100

    adjust responsiveness

--- JS_Scripts/script.js
+++ JS_Scripts/script.js
@@ -1,8 +1,20 @@
+var sidebarWidth = "-15%";
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openSidebar() {
   document.getElementsByClassName("sidebar")[0].style.left = "0%";
 }
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeSidebar() {
-  document.getElementsByClassName("sidebar")[0].style.left = "-15%";
+  document.getElementsByClassName("sidebar")[0].style.left = sidebarWidth;
 }
+
+function sidebarAssign(sidebarResize) {
+  if (sidebarResize.matches) {
+    sidebarWidth = "-40%";
+  } else {
+    sidebarWidth = "-15%";
+  }
+}
+var sidebarResize = window.matchMedia("(max-width: 500px)");
+sidebarAssign(sidebarResize); // Call listener function at run time
+sidebarResize.addListener(sidebarAssign);
