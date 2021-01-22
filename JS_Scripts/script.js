d98a2555efcca6a5c49c00162e4485465e262cad
10GGGGGGGGGG <47125167+10GGGGGGGGGG@users.noreply.github.com>
2021-01-22 20:14:29 +0100

    finalizado el reproductor y añadido carrusel

--- JS_Scripts/script.js
+++ JS_Scripts/script.js
@@ -18,3 +18,36 @@ function sidebarAssign(sidebarResize) {
 var sidebarResize = window.matchMedia("(max-width: 500px)");
 sidebarAssign(sidebarResize); // Call listener function at run time
 sidebarResize.addListener(sidebarAssign);
+
+function openPlayer() {
+  const bottom = document.querySelector(".player").style.bottom;
+  if (bottom == "-70px" || bottom == "") {
+    document.getElementById("openPlayer").innerHTML = "keyboard_arrow_down";
+    document.getElementById("trapezoid").style.bottom = "70px";
+    document.getElementsByClassName("player")[0].style.bottom = "0px";
+  } else {
+    document.getElementById("openPlayer").innerHTML = "keyboard_arrow_up";
+    document.getElementById("trapezoid").style.bottom = "0px";
+    document.getElementsByClassName("player")[0].style.bottom = "-70px";
+  }
+}
+
+var vol_slider = document.getElementById("vol_slider");
+var vol_output = document.getElementById("vol_output");
+vol_output.innerHTML = vol_slider.value + "%";
+
+vol_slider.oninput = function () {
+  vol_output.innerHTML = this.value + "%";
+};
+
+new Splide(".splide", {
+  perPage: 3,
+  cover: true,
+  height: "12rem",
+  lazyLoad: "nearby",
+  gap: "10px",
+  pagination: false,
+  breakpoints: {
+    height: "8rem",
+  },
+}).mount();
